3addaa0252f08e83188ddc4b55ed753abf84edd4
Raya Chang <raya11600@gmail.com>
2015-10-01 22:32:28 +0800

    Modify TFSF

--- TFSF.cpp
+++ TFSF.cpp
@@ -1,12 +1,51 @@
 #include <stdio.h>
 #include "TFSF.h"
+#include "Parameter.h"
 using namespace std;
 
 TFSF::TFSF(int CPMLGrid) {
+
 	TFSFGrid = CPMLGrid + 5;
+
+	startZ = TFSF - 1;
 }
 
 TFSF::~TFSF() {
+
+}
+
+void TFSF::AddTfsf2Sides_E(double ***Ex, double ***Ey, double ***Ez, double *hy) {
+	Parameter param;
+	int SIZE_X = param.SIZE_X;
+	int SIZE_Y = param.SIZE_Y;
+	int SIZE_Z = param.SIZE_Z;
+
+	int k = startZ;
+	for( int i = 0; i < SIZE_X-1; i++ )
+		for( int j = 1; j < SIZE_Y-1; j++ ) { 
+			Ex[i][j][k] += (param.dt/param.dx/param.eps0) * hy[k];
+		}
+
+}
+
+void TFSF::AddTfsf2Sides_H(double ***Hx, double ***Hy, double ***Hz, double *ex) {
+	Parameter param;
+	int SIZE_X = param.SIZE_X;
+	int SIZE_Y = param.SIZE_Y;
+	int SIZE_Z = param.SIZE_Z;
+
+	k=startZ; 
+	for( int i = 0; i < SIZE_X-1; i++ )
+		for( int j = 0; j < SIZE_Y; j++ ) {
+		  	Hy[i][j][k] += (param.dt/param.dx/param.mu0) * ex[k];
+		}
+}
+
+void TFSF::AddTfsf6Sides_E() {
+
+}
+
+void TFSF::AddTfsf6Sides_H() {
 
 }
 
--- TFSF.h
+++ TFSF.h
@@ -5,10 +5,20 @@ class TFSF {
 private:
 	int TFSFGrid;
 
+	int startX;
+	int startY;
+	int startZ;
+
 public:
 	TFSF();
 	~TFSF();
 
+	void AddTfsf2Sides_E(double ***Ex, double ***Ey, double ***Ez, double *hy);
+	void AddTfsf2Sides_H(double ***Hx, double ***Hy, double ***Hz, double *ex);
+
+	void TFSF::AddTfsf6Sides_E();
+	void TFSF::AddTfsf6Sides_H();
+	
 	int getTFSFGrid();
 };
 
